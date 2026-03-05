e41a0ed54808bb230b9f12438810639d6636baaf
Zmrrr11 <zamirmarca90@gmail.com>
2026-03-04 20:48:14 -0400

    creacion componentes y codigo

--- src/app/componentes/campo-entrada/campo-entrada.ts
+++ src/app/componentes/campo-entrada/campo-entrada.ts
@@ -1,11 +1,51 @@
+// campo-entrada.component.ts
 import { Component } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-campo-entrada',
-  imports: [],
-  templateUrl: './campo-entrada.html',
-  styleUrl: './campo-entrada.css',
+  standalone: true,
+  template: 'sadas', 
 })
 export class CampoEntrada {
 
+  /**
+   * Crea un campo de texto 3D estilizado
+    @param yPos 
+    @param etiqueta 
+   */
+  crearInput(yPos: number, etiqueta: string): THREE.Group {
+    const grupoInput = new THREE.Group();
+
+    const geometriaCaja = new THREE.PlaneGeometry(3.5, 0.6);
+    const materialCaja = new THREE.MeshPhongMaterial({
+      color: 0x111111,
+      transparent: true,
+      opacity: 0.7,
+      side: THREE.DoubleSide
+    });
+    const fondo = new THREE.Mesh(geometriaCaja, materialCaja);
+    const bordesGeo = new THREE.EdgesGeometry(geometriaCaja);
+    const bordesMat = new THREE.LineBasicMaterial({ color: 0x00ffff }); // Color cian neón
+    const contorno = new THREE.LineSegments(bordesGeo, bordesMat);
+
+    // 3. Icono Simulado (Un pequeño cuadrado al inicio)
+    const iconoGeo = new THREE.PlaneGeometry(0.3, 0.3);
+    const iconoMat = new THREE.MeshBasicMaterial({ color: 0x00ffff });
+    const icono = new THREE.Mesh(iconoGeo, iconoMat);
+    icono.position.x = -1.4; // Posicionado a la izquierda del campo
+
+    // Unir todo
+    grupoInput.add(fondo, contorno, icono);
+    grupoInput.position.set(0, yPos, 0.1); // Un poco adelantado para evitar z-fighting con el fondo 360
+    
+    // Datos para el Raycaster (Diferenciación de IA)
+    grupoInput.userData = { 
+      tipo: 'input', 
+      id: etiqueta,
+      colorOriginal: 0x00ffff 
+    };
+
+    return grupoInput;
+  }
 }
--- src/app/componentes/visor-escena/visor-escena.ts
+++ src/app/componentes/visor-escena/visor-escena.ts
@@ -29,3 +29,4 @@ export class CampoEntrada {
     return grupo;
   }
 }
+//pruba par subir
